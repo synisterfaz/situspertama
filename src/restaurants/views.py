7b15a33d885e3aea81e8d03c2574a6adeff1da93
synisterfaz <neomuslimz@gmail.com>
2018-02-11 21:04:35 +0700

    ketiga

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView,DetailView
 from .models import RestaurantLocation
 
 # Create your views here.
@@ -13,6 +14,61 @@ def restaurant_listview(request):
 		"object_list":queryset
 	}
 	return render(request,template_name,context)
+
+class RestaurantListView(ListView):
+	#template_name = 'restaurants/restaurant_list.html'
+
+	def get_queryset(self):
+		
+		slug =self.kwargs.get("slug")
+		if slug:
+			queryset=RestaurantLocation.objects.filter(
+				Q(category__iexact=slug) |
+				Q(category__icontains=slug)
+
+				)
+		else:
+			queryset=RestaurantLocation.objects.all()
+		return queryset
+
+class RestaurantDetailView(DetailView):
+	queryset=RestaurantLocation.objects.all()
+	
+	#def get_context_data(self,*args,**kwargs):
+	#	print(self.kwargs)
+	#	context = super(RestaurantDetailView, self).get_context_data(*args,**kwargs)
+	#	print(context)
+	#	return context
+
+	#def get_object(self,*args,**kwargs):
+	#	rest_id = self.kwargs.get('rest_id')
+	#	obj = get_object_or_404(RestaurantLocation, id=rest_id) # mereplace pk dengan rest_id
+	#	return obj
+
+
+#class MexicanListView(ListView):
+	#queryset=RestaurantLocation.objects.filter(category__iexact='mexican')
+	#template_name = 'restaurants/restaurant_list.html'
+
+#class AsianFusionListView(ListView):
+	#queryset=RestaurantLocation.objects.filter(category__iexact='asian fusion')
+	#template_name = 'restaurants/restaurant_list.html'
+
+#class SearchListView(ListView):
+#	template_name = 'restaurants/restaurant_list.html'
+
+#	def get_queryset(self):
+#		print(self.kwargs)
+#		slug =self.kwargs.get("slug")
+#		if slug:
+#			queryset=RestaurantLocation.objects.filter(
+#				Q(category__iexact=slug) |
+#				Q(category__icontains=slug)
+
+#				)
+#		queryset=RestaurantLocation.objects.none()
+#		return queryset
+
 
 #def home(request):
 	#return HttpResponse("Hello")
--- src/situspertama/urls.py
+++ src/situspertama/urls.py
@@ -17,12 +17,22 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import restaurant_listview
-
+from restaurants.views import (
+	restaurant_listview,
+	RestaurantListView,
+	RestaurantDetailView,
+	#MexicanListView,
+	#AsianFusionListView,
+	#SearchListView,
+)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
-    url(r'^restaurants/$', restaurant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    #url(r'^restaurants/(?P<slug>\w+)/$',RestaurantListView.as_view()),
+    #url(r'^restaurants/(?P<rest_id>\w+)/$',RestaurantDetailView.as_view()),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$',RestaurantDetailView.as_view()),
+    #url(r'^restaurants/asian/$', AsianFusionListView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^kontak/$', TemplateView.as_view(template_name="kontak.html")),
 ]
